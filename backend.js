9cca3cd5628b09e68b51b6dc1397626d2272f8f4
Maël Gruand <maelgruand1@yahoo.com>
2025-01-02 19:21:14 +0100

    modif

--- backend.js
+++ backend.js
@@ -1,18 +1,52 @@
-// config firebase
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Configuration Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCu9EmzECDjAtvy7Xrgre-_JfpU-YoCmFY",
-  authDomain: "reseausocial-2025.firebaseapp.com",
-  projectId: "reseausocial-2025",
-  storageBucket: "reseausocial-2025.firebasestorage.app",
-  messagingSenderId: "418687081310",
-  appId: "1:418687081310:web:53e733da1caf1e50c7cdcf"
-};
+    apiKey: "AIzaSyCu9EmzECDjAtvy7Xrgre-_JfpU-YoCmFY",
+    authDomain: "reseausocial-2025.firebaseapp.com",
+    projectId: "reseausocial-2025",
+    storageBucket: "reseausocial-2025.appspot.com",
+    messagingSenderId: "418687081310",
+    appId: "1:418687081310:web:53e733da1caf1e50c7cdcf"
+  };
+  
+  // Initialisation de Firebase
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  
+const signupForm = document.getElementById("signup-form");
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+signupForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("signup-username").value;
+    const password = document.getElementById("signup-password").value;
+
+    auth.createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Utilisateur inscrit
+            alert("Inscription réussie !");
+            console.log("Utilisateur inscrit :", userCredential.user);
+        })
+        .catch((error) => {
+            console.error("Erreur :", error.message);
+            alert(`Erreur : ${error.message}`);
+        });
+});
+const loginForm = document.getElementById("login-form");
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("login-username").value;
+    const password = document.getElementById("login-password").value;
+
+    auth.signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Connexion réussie
+            alert("Connexion réussie !");
+            console.log("Utilisateur connecté :", userCredential.user);
+        })
+        .catch((error) => {
+            console.error("Erreur :", error.message);
+            alert(`Erreur : ${error.message}`);
+        });
+});
